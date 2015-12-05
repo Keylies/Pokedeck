e331554813589f5503cafc9292c0278e0d0b3ec2
Keylies <lemenct@gmail.com>
2015-12-05 16:35:00 +0100

    GUI draft

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Controller {
 
+public class Controller implements ActionListener {
+	
+	private Menu menu;
+	
+	public Controller(Menu m) {
+		menu = m;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(menu.getData());
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
+import javax.swing.JFrame;
 
-public class Main {
+
+public class Main extends JFrame {
 
 	public static void main(String[] args) {
-		Card grassCard = new Energy("Grass");
-		Card bulbizarre = new Pokemon("Bulbizarre", "Bulbe bizarre", 1, "Grass", 100);
-		Card sacha = new Trainer("Sacha", "Le meilleur dresseur", 200, "Supporter", "Gogogogo");
 		
-		Player p1 = new Player("Clém");
+		Menu menu = new Menu();
+		menu.constructGUI();
 		
-		p1.addToDeck(grassCard);
-		p1.addToDeck(bulbizarre);
-		p1.addToDeck(sacha);
-		
-		bulbizarre.changeText("BUUUULBI");
-		
-		System.out.println(p1.toString());
+//		Card grassCard = new Energy("Grass");
+//		Card bulbizarre = new Pokemon("Bulbizarre", "Bulbe bizarre", 1, "Grass", 100);
+//		Card sacha = new Trainer("Sacha", "Le meilleur dresseur", 200, "Supporter", "Gogogogo");
+//		
+//		Player p1 = new Player("Clém");
+//		
+//		p1.addToDeck(grassCard);
+//		p1.addToDeck(bulbizarre);
+//		p1.addToDeck(sacha);
+//		
+//		bulbizarre.changeText("BUUUULBI");
+//		
+//		System.out.println(p1.toString());
 	}
 
 }
